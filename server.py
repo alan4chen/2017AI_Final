6db0623f62298d382c39b02359eae9ede0ecbad8
Mark Yan <b01502069@ntu.edu.tw>
2018-01-02 16:12:28 +0800

    filter not text

--- server.py
+++ server.py
@@ -43,6 +43,9 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
+                    if not messaging_event["message"].get("text"):
+                        pass
+                    
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
